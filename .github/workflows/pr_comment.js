ce1082db533e9f962cefb910c63c1d11305a106c
mstssk <mstssk+github@gmail.com>
2025-02-12 00:56:42 +0900

    結局権限がないと取得できないのかも？ ちゃんとPRのイベントの中で実行してみないとだめかも

--- .github/workflows/pr_comment.js
+++ .github/workflows/pr_comment.js
@@ -3,7 +3,7 @@ module.exports = async ({ github, context }) => {
   const { data: comments } = await github.rest.pulls.listReviewComments({
     owner: "mstssk",
     repo: "sandbox",
-    pull_number: 1,
+    pull_number: 1, // TODO ここで書いても、結局権限がないと取得できないのかも？ ちゃんとPRのイベントの中で実行してみないとだめかも
   });
   console.log("comments.length", comments.length);
   console.log(comments);
