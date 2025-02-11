7ea7bb54344a6032968a350654b70b1f6bcd7b46
mstssk <mstssk+github@gmail.com>
2025-02-12 00:46:17 +0900

    wip

--- .github/workflows/pr_comment.js
+++ .github/workflows/pr_comment.js
@@ -1,4 +1,9 @@
-module.exports = ({ octokit, context }) => {
+module.exports = async ({ github, context }) => {
   console.log("test");
-  return context.payload.client_payload.value;
+  const { data: comments } = await octokit.rest.pulls.listReviewComments({
+    owner: "mstssk",
+    repo: "sandbox",
+    pull_number: 1,
+  });
+  return comments;
 };
